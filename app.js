48c5f070fa6c805126949bf5bb9eb1fa3b11f85a
Dawid Olczak <dawid.a.olczak@gmail.com>
2021-11-21 20:02:36 +0100

    Created basic files and structure

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');             // podłączenie do głównego pliku expressa
+const hbs = require('express-handlebars');       // zaimplementowanie handlebarsów do aplikacji naszej
+const {urlencoded} = require("express");
+const {db} = require('./utils/db');
+
+const app = express();
+
+app.use(express.urlencoded({
+        extended: true,
+    }));
+
+app.use(express.static('public'));     // używanie plików statycznych będących w folderze "public"
+app.use(express.json());
+app.engine('.hbs', hbs({             // ustawienie silnika widoków na handlebars
+    extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
+
+
+app.listen(3000, 'localhost', () => {           // ustawienie nasłuchiwania na port 3000
+    console.log('App started http://localhost:3000')
+});
---
+++ utils/db.js
@@ -0,0 +1,7 @@
+const {readFile, writeFile} = require('fs').promises; // bierzemy dwie metody zapis i odczyt pliku z modułu fs. wersja promisowa
+const {join} = require('path'); //bierzemy metodę join z modułu path
+
+
+module.exports = {
+    db
+}

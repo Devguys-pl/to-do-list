6b5891b54447db2e7ed903da9196c3f159ec2ee3
Dawid Olczak <dawid.a.olczak@gmail.com>
2021-11-22 21:00:09 +0100

    Updated

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express');             // podłączenie do głównego pliku expressa
-const hbs = require('express-handlebars');       // zaimplementowanie handlebarsów do aplikacji naszej
+const exphbs  = require('express-handlebars');      // zaimplementowanie handlebarsów do aplikacji naszej
 const {urlencoded} = require("express");
-const {db} = require('./utils/db');
+// const {db} = require('./utils/db');
 
 const app = express();
-
+const hbs = exphbs.create({ /* config */ });
 app.use(express.urlencoded({
         extended: true,
     }));
 
 app.use(express.static('public'));     // używanie plików statycznych będących w folderze "public"
 app.use(express.json());
-app.engine('.hbs', hbs({             // ustawienie silnika widoków na handlebars
-    extname: '.hbs',
-}));
-app.set('view engine', '.hbs');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
 app.listen(3000, 'localhost', () => {           // ustawienie nasłuchiwania na port 3000
